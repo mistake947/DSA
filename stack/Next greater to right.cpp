cd98f96522fd784f67b75261578fc6ee80464778
mistake947 <jcvikas947@gmail.com>
2022-08-11 07:31:44 +0530

    Create Next greater to right.cpp

---
+++ stack/Next greater to right.cpp
@@ -0,0 +1,15 @@
+       vector<int>ans(n);
+        stack<int>st;
+        for(int i=0;i<nums.size();i++)
+        {
+            while(!st.empty() and nums[st.top()]<nums[i])   //Greater Mai current elenment nums[i]
+                                                             // ko greter rakhenge ,smaller hota to
+                                                            //nums[i] ko small rakahte
+            {
+                int idx=st.top();
+                st.pop();           // right ke liye yaha calculate hota hai
+                ans[idx]=nums[i];
+            }
+            st.push(i);     // left k liye yaha calculate hota hai
+        }
+        return ans;
