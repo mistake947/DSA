aa174b0b1c342f7a35e3a43a5251ac52733a56ad
mistake947 <84361502+mistake947@users.noreply.github.com>
2023-03-18 13:23:57 +0530

    Update Longest_Increasing_subsuquece.cpp

--- dp/Longest_Increasing_subsuquece.cpp
+++ dp/Longest_Increasing_subsuquece.cpp
@@ -37,3 +37,38 @@ int main() {
 
 
 // ........................................................
+
+// NUMBER OF LONGEST INCREASING SUBSQUENCE   N*N
+
+int findNumberOfLIS(vector<int>& nums) {
+        int n=nums.size(),ans=0, maxxl=1;
+        vector<int>dp(n,1);
+        vector<int>count(n,1);
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(nums[i]>nums[j]) 
+                {
+                    if(dp[i]<dp[j]+1)
+                    {
+                         dp[i]=dp[j]+1;
+                        count[i]=count[j];
+                    }
+                    else if(dp[i]==dp[j]+1)count[i]+=count[j];
+                   
+                    
+                }
+                maxxl=max(maxxl,dp[i]);
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(dp[i]==maxxl)ans+=count[i];
+        }
+      return ans;
+        
+    }
+
+
+// .......................................................................................
